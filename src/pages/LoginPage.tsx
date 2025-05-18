2ee276654385a959596700078f032903d3869174
nikam3322 <nikam3322@gmail.com>
2025-05-18 11:14:59 +0200

    feat: add .env file

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/LoginPage.scss';
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
